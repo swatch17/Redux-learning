378d8ff9b0196d53d775ce6ff85dc01bd7380582
Dean <swatch615@gmail.com>
2019-08-22 15:19:42 +0800

    添加redux-thunk

--- demo3/src/store/index.js
+++ demo3/src/store/index.js
@@ -3,7 +3,8 @@ import thunk from 'redux-thunk'
 import reducer from './reducer'
 
 // 创建一个增强函数
-const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION__ ?window.__REDUX_DEVTOOLS_EXTENSION__({}):compose
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
 const enhancer = composeEnhancers(applyMiddleware(thunk))
 
 //创建数据库存储仓库
